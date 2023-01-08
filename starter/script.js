c0a8f484bb56ed15ab5832c8c4493b943aca8f1f
Mirella <mirella.dimichino@gmail.com>
2023-01-08 14:54:41 +0000

    added password options/prompts and loops

--- starter/script.js
+++ starter/script.js
@@ -88,8 +88,46 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+//added 2 more variables storing the prompts + pass length
+
+var passwordLength = 0;
+var Userchoice = "";
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var passwordLength = parseInt(prompt("Choose your password length: 10-64 characters"))
+  if (passwordLength < 10 || passwordLength > 64) {
+    alert("You must choose only numbers 10-64!")
+    return getPasswordOptions
+  }
+
+  //Declared all users options and prompts
+
+  var isSpecialchar = prompt("would you include  specialcharacters?")
+  var isUppercase = prompt ("Would you include uppercase letters?")
+  var isLowercase = prompt ("Would you include lower case letters?")
+  var isNumber = prompt ("Would you include numbers?")
+
+  //Loops for user inputs, using concat method to attach the results
+
+  if (isSpecialchar === true) {
+    Userchoice = Userchoice.concat(specialCharacters)
+  }
+
+  if (isUppercase ===true) {
+    Userchoice = Userchoice.concat(upperCasedCharacters)
+  }
+
+  if(isLowercase ===true) {
+    Userchoice = Userchoice.concat(lowerCasedCharacters)
+  }
+
+  if(isNumber ===true) {
+    Userchoice = Userchoice.concat(numericCharacters)
+  }
+
+  console.log(Userchoice);
+
 
 }
 
